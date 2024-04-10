5f4a7d7ba921bef65b29f34911bc0483463d54c6
coryswynn <wynn.cory@gmail.com>
2024-04-10 14:31:02 -0700

    Add files via upload

---
+++ client-view.js
@@ -0,0 +1,65 @@
+ZoomMtg.preLoadWasm()
+ZoomMtg.prepareWebSDK()
+
+var authEndpoint = ''
+var sdkKey = ''
+var meetingNumber = '123456789'
+var passWord = ''
+var role = 0
+var userName = 'JavaScript'
+var userEmail = ''
+var registrantToken = ''
+var zakToken = ''
+var leaveUrl = 'https://zoom.us'
+
+function getSignature() {
+  fetch(authEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      meetingNumber: meetingNumber,
+      role: role
+    })
+  }).then((response) => {
+    return response.json()
+  }).then((data) => {
+    console.log(data)
+    startMeeting(data.signature)
+  }).catch((error) => {
+  	console.log(error)
+  })
+}
+
+function startMeeting(signature) {
+
+  document.getElementById('zmmtg-root').style.display = 'block'
+
+  ZoomMtg.init({
+    leaveUrl: leaveUrl,
+    patchJsMedia: true,
+    success: (success) => {
+      console.log(success)
+      ZoomMtg.join({
+        signature: signature,
+        sdkKey: sdkKey,
+        meetingNumber: meetingNumber,
+        passWord: passWord,
+        userName: userName,
+        userEmail: userEmail,
+        tk: registrantToken,
+        zak: zakToken,
+        success: (success) => {
+          console.log(success)
+        },
+        error: (error) => {
+          console.log(error)
+        },
+      })
+    },
+    error: (error) => {
+      console.log(error)
+    }
+  })
+}
